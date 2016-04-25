6d02d23581d208e131611567a2088572969959a5
qizt <qiztzone@126.com>
2016-04-24 23:59:54 -0500

    Fix injector bugs, change surefire timeout.

--- MutationGenerator/src/MutationInjector.java
+++ MutationGenerator/src/MutationInjector.java
@@ -8,42 +8,69 @@ import java.util.ArrayList;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.ClassVisitor;
 
 public class MutationInjector {
 	public static void main(String[] args) throws Exception {
 		ArrayList<File> classes = new ArrayList<File>();
 		File ParentDirectory = new File(System.getProperty("user.dir")).getParentFile();
-		ClassFiles.getClasses(new File(ParentDirectory.getAbsolutePath()+"/target/classes"), classes);
+		getClasses(new File(ParentDirectory.getAbsolutePath()+"/target/classes"), classes);
 		
 		if (args[0].equals("MATH")) {
-			System.out.println("Injecting MATH mutator.");
-			ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-			MATHCv ca = new MATHCv(cw);
-			inject(classes, cw, ca);
+			System.out.println("========== MATH ==========");
+			injectMATH(classes);
 		} 
 		else if (args[0].equals("INCREMENTS")) {
-			System.out.println("Injecting INCREMENTS mutator.");
-			ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-			INCREMENTSCv ca = new INCREMENTSCv(cw);
-			inject(classes, cw, ca);
+			System.out.println("======= INCREMENTS =======");
+			injectINCREMENTS(classes);
 		}
 
 	}
 	
-	private static void inject(ArrayList<File> classes,ClassWriter cw, ClassVisitor ca) throws IOException {
+	private static void injectINCREMENTS(ArrayList<File> classes) throws IOException {
 		for (File f : classes) {
 			FileInputStream is = new FileInputStream(f);
 			try {
 				ClassReader cr = new ClassReader(is);
-				/* YIELD */
+				ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+				INCREMENTSCv ca = new INCREMENTSCv(cw);
 				cr.accept(ca, 0);
 				FileOutputStream fos = new FileOutputStream(f.getAbsoluteFile());
 				fos.write(cw.toByteArray());
 				fos.close();	
+				System.out.println("Injecting INCREMENTS mutators into "+ f.getName() + ",");
 			} catch (IllegalArgumentException e) {
 				System.out.println("Skiped a none-class file: "+ f.getName());
 			}
 		}
+	}
+	
+	private static void injectMATH(ArrayList<File> classes) throws IOException {
+		for (File f : classes) {
+			FileInputStream is = new FileInputStream(f);
+			try {
+				ClassReader cr = new ClassReader(is);
+				ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+				MATHCv ca = new MATHCv(cw);
+				cr.accept(ca, 0);
+				FileOutputStream fos = new FileOutputStream(f.getAbsoluteFile());
+				fos.write(cw.toByteArray());
+				fos.close();	
+				System.out.println("Injecting MATH mutators into "+ f.getName() + ",");
+			} catch (IllegalArgumentException e) {
+				System.out.println("Skiped a none-class file: "+ f.getName());
+			}
+		}
+	}
+	
+	public static void getClasses(File directory, ArrayList<File> files) throws Exception {
+	    // get all the files from a directory
+	    File[] fList = directory.listFiles();
+	    for (File file : fList) {
+	        if (file.isFile() && !file.isHidden()) {
+	            files.add(file);
+	        } else if (file.isDirectory()) {
+	        	getClasses(file, files);
+	        }
+	    }
 	}
 }
--- geo/MutatedShadow/shadow.sh
+++ geo/MutatedShadow/shadow.sh
@@ -5,31 +5,37 @@ totalShadow=0
 successShadow="SUCCESS"
 reportShadow="MutatedShadow/report"
 
-mkdir backup
-mkdir backup/classes
-mkdir report
+mkdir backup &> /dev/null
+mkdir backup/classes &> /dev/null
+mkdir report &> /dev/null
 
 cd ..
 
+echo Building the project...
+
 mvn test > MutatedShadow/report/0-origin.txt
+
+echo Build report recorded in MutatedShadow/report/0-origin.txt
 
 cp -R target/classes/ MutatedShadow/backup/classes/
 
-echo === MutatedShadow === > $reportShadow/summary.txt
+echo ========== MutatedShadow ========== > $reportShadow/summary.txt
 
 # MATH
 cd MutatedShadow
 java -cp .:./asm-all-5.1.jar MutationInjector MATH
 cd ..
+echo Running tests...
 mvn surefire:test -Dsurefire.timeout=2 > $reportShadow/1-MATH.txt
 totalShadow=`expr $totalShadow + 1`
 grep $successShadow $reportShadow/1-MATH.txt > $reportShadow/.grep
 if [ $? -eq 0 ] ; then
-  echo MATH: SUCCESS >> $reportShadow/summary.txt
+  echo 1. MATH: SUCCESS >> $reportShadow/summary.txt
 else
-  echo MATH: FAIL >> $reportShadow/summary.txt
+  echo 1. MATH: FAIL >> $reportShadow/summary.txt
 	killedShadow=`expr $killedShadow + 1`
 fi
+echo Testing report recorded in MutatedShadow/report/1-MATH.txt
 rm -rf target/classes/
 cp -R MutatedShadow/backup/classes/ target/classes/
 
@@ -37,15 +43,17 @@ cp -R MutatedShadow/backup/classes/ target/classes/
 cd MutatedShadow
 java -cp .:./asm-all-5.1.jar MutationInjector INCREMENTS
 cd ..
+echo Running tests...
 mvn surefire:test -Dsurefire.timeout=2 > $reportShadow/2-INCREMENTS.txt
 totalShadow=`expr $totalShadow + 1`
 grep $successShadow $reportShadow/2-INCREMENTS.txt > $reportShadow/.grep
 if [ $? -eq 0 ] ; then
-  echo MATH: SUCCESS >> $reportShadow/summary.txt
+  echo 2. INCREMENTS: SUCCESS >> $reportShadow/summary.txt
 else
-  echo MATH: FAIL >> $reportShadow/summary.txt
+  echo 2. INCREMENTS: FAIL >> $reportShadow/summary.txt
 	killedShadow=`expr $killedShadow + 1`
 fi
+echo Testing report recorded in MutatedShadow/report/2-INCREMENTS.txt
 rm -rf target/classes/
 cp -R MutatedShadow/backup/classes/ target/classes/
 
@@ -53,3 +61,4 @@ cp -R MutatedShadow/backup/classes/ target/classes/
 
 echo $totalShadow mutations total. >> $reportShadow/summary.txt
 echo $killedShadow mutations are killed. >> $reportShadow/summary.txt
+echo Summary report recorded in MutatedShadow/report/summary.txt
