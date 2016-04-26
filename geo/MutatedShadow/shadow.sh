a3a74184a336770cdbb8958a6e3e4b40c2cc19ff
qizt <qiztzone@126.com>
2016-04-26 12:45:37 -0500

    2 mutators releasable version.

--- geo/MutatedShadow/shadow.sh
+++ geo/MutatedShadow/shadow.sh
@@ -57,8 +57,27 @@ echo Testing report recorded in MutatedShadow/report/2-INCREMENTS.txt
 rm -rf target/classes/
 cp -R MutatedShadow/backup/classes/ target/classes/
 
+# INCREMENTS
+cd MutatedShadow
+java -cp .:./asm-all-5.1.jar MutationInjector INCREMENTS
+cd ..
+echo Running tests...
+mvn surefire:test -Dsurefire.timeout=2 > $reportShadow/2-INCREMENTS.txt
+totalShadow=`expr $totalShadow + 1`
+grep $successShadow $reportShadow/2-INCREMENTS.txt > $reportShadow/.grep
+if [ $? -eq 0 ] ; then
+  echo 2. INCREMENTS: SUCCESS >> $reportShadow/summary.txt
+else
+  echo 2. INCREMENTS: FAIL >> $reportShadow/summary.txt
+	killedShadow=`expr $killedShadow + 1`
+fi
+echo Testing report recorded in MutatedShadow/report/2-INCREMENTS.txt
+rm -rf target/classes/
+cp -R MutatedShadow/backup/classes/ target/classes/
+
 
 
 echo $totalShadow mutations total. >> $reportShadow/summary.txt
 echo $killedShadow mutations are killed. >> $reportShadow/summary.txt
 echo Summary report recorded in MutatedShadow/report/summary.txt
+rm -rf MutatedShadow/backup
